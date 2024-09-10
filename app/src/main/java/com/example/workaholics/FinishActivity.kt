7a27d62fc59c45aa4f95a5dedf5767df22f0df50
Priyanshuanand1 <Rajyansh@gmail.com>
2024-09-10 23:50:01 +0530

    first commit

---
+++ app/src/main/java/com/example/workaholics/FinishActivity.kt
@@ -0,0 +1,28 @@
+package com.example.workaholics
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.workaholics.databinding.ActivityFinishBinding
+
+class FinishActivity : AppCompatActivity() {
+    private var binding: ActivityFinishBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFinishBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarFinishActivity)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarFinishActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        binding?.btnFinish?.setOnClickListener {
+            finish()
+        }
+    }
+}
